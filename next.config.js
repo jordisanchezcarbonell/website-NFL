9fb918d3e0b415753a61f880a739bf7e024bddb2
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2022-12-09 16:05:37 +0100

    Add tailwind , navbar.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 
-module.exports = nextConfig
+    domains: ["tailwindui.com", "images.unsplash.com"],
+  },
+};
+
+module.exports = nextConfig;
